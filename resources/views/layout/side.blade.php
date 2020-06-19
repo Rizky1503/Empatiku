7e78af994002182a28240eee2a72c64ced08b92b
rizky1503 <permatarizky@gmail.com>
2020-06-19 14:38:18 +0700

    100%

--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -71,7 +71,7 @@
             </a>
           </li> 
           <li class="nav-item">
-            <a href="{{ route ('MitraVendor.TambahMitraVendor') }}" class="nav-link">
+            <a href="{{ route ('Orderproduk.ListPemesanan') }}" class="nav-link">
               <i class="nav-icon fas fa-plus"></i>
               <p>
                 Lihat Pemesan
